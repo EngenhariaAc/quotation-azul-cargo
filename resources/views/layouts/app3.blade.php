258a6b4d1ddd24bb0e50c7e7ebc44e99b30cd036
Gabriel Cabral <jgabrielppc@gmail.com>
2025-09-22 10:19:39 -0300

    ajustando títulos nos layouts

--- resources/views/layouts/app3.blade.php
+++ resources/views/layouts/app3.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Acesso BI</title>
+    <title>Cotação</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
